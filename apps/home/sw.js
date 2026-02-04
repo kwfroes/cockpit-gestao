63fd9ed64012acbc4787203daa00a5bba2d2a99a
Kevin Fróes <kwfroes@gmail.com>
2026-02-04 00:23:12 -0300

    update version

--- apps/home/sw.js
+++ apps/home/sw.js
@@ -1,38 +1,40 @@
-const CACHE_NAME = 'cockpit-cache-v4';
+const CACHE_NAME = "cockpit-cache-v6";
 const ASSETS_TO_CACHE = [
-  './',                 // A raiz
-  './index.html',       // O HTML
-  './script.js',        // O cérebro
-  './qualificacao_tecnica.json', // Os dados (importante!)
-  './manifest.json',
+  "./", // A raiz
+  "./index.html", // O HTML
+  "./script.js", // O cérebro
+  "./qualificacao_tecnica.json", // Os dados (importante!)
+  "./manifest.json",
   // Adicione aqui qualquer CSS externo ou imagem que vc use localmente
 ];
 
 // 1. Instalação: Baixa os arquivos para o celular
-self.addEventListener('install', (e) => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
-    })
+    }),
   );
 });
 
 // 2. Requisição: Tenta pegar do cache primeiro, se falhar, vai pra internet
-self.addEventListener('fetch', (e) => {
+self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => {
       return response || fetch(e.request);
-    })
+    }),
   );
 });
 
 // 3. Atualização: Limpa caches antigos quando muda a versão
-self.addEventListener('activate', (e) => {
+self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== CACHE_NAME) return caches.delete(key);
-      }));
-    })
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        }),
+      );
+    }),
   );
 });
